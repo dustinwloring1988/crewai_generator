c1d65f1d7462e3b179941e77681ccb7e0f46cbc2
Dustin Loring <dustinwloring1988@gmail.com>
2024-05-14 17:12:16 -0400

    Update main.py

--- src/crewai_generator/main.py
+++ src/crewai_generator/main.py
@@ -5,6 +5,6 @@ from crewai_generator.crew import CrewaiGeneratorCrew
 def run():
     # Replace with your inputs, it will automatically interpolate any tasks and agents information
     inputs = {
-        'topic': 'AI LLMs'
+        'library_name': 'https://github.com/joaomdmoura/crewAI-tools/blob/main/crewai_tools/tools/code_docs_search_tool/README.md'
     }
     CrewaiGeneratorCrew().crew().kickoff(inputs=inputs)
